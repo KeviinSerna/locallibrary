3e909c82bac99202fa46a0c33ec9ba31441fc3d5
KevinSerna <kevinsernap@gmail.com>
2023-06-15 16:46:13 -0500

    Change: modified files

--- catalog/views.py
+++ catalog/views.py
@@ -1,25 +1,38 @@
 from django.shortcuts import render
-
 # Create your views here.
-from .models import Book
-from .models import Author
-from .models import BookInstance
-from .models import Genre
+
+from .models.book import Book
+from .models.author import Author
+from .models.book_instance import BookInstance
+from .models.genre import Genre
+
 
 def index(request):
-    """
-    Función vista para la página inicio del sitio.
-    """
-    # Genera contadores de algunos de los objetos principales
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
-    # Libros disponibles (status = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # El 'all()' esta implícito por defecto.
+    """View function for home page of site."""
+    search_word = request.GET.get('search_word', '').lower()
 
-    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
-    return render(
-        request,
-        'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
-    )
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_genres = Genre.objects.filter(name__icontains=search_word).count()
+    num_books = Book.objects.filter(title__icontains=search_word).count()
+    
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres' : num_genres
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
